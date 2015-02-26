84fd419440c18392db99fd09ef741e4bfb2a4b9f
jto <boudhevil@gmail.com>
2015-02-26 17:39:31 +0100

    syntax

--- app/Monitored.scala
+++ app/Monitored.scala
@@ -8,15 +8,30 @@ trait Monitored[A] {
 	def apply(c: Context) = f(c)
 }
 
+trait OptionTMonitored[F[_], A] extends Monitored[F[Option[A]]] {
+	def T = Monitored.optT[F, A](this)
+}
+
+trait ListTMonitored[F[_], A] extends Monitored[F[List[A]]] {
+	def T = Monitored.listT[F, A](this)
+}
+
 object Monitored {
+	implicit def toOptionT[F[_], A](m: Monitored[F[Option[A]]]) = new OptionTMonitored[F, A] {
+		val f = m.f
+	}
+
+	implicit def toListT[F[_], A](m: Monitored[F[List[A]]]) = new ListTMonitored[F, A] {
+		val f = m.f
+	}
 
 	private def toT[F[_], G[_], A, T[_[_], _]](f: F[G[A]] => T[F, A])(m: Monitored[F[G[A]]]): Kleisli[({ type λ[α] = T[F, α] })#λ, Context, A] = {
 		type Trans[α] = T[F, α]
 		Kleisli(m.f).mapK[Trans, A](f)
 	}
 
-	def optT[A] = toT[Future, Option, A, OptionT](OptionT.apply _) _
-	def listT[A] = toT[Future, List, A, ListT](ListT.apply _) _
+	def optT[F[_], A] = toT[F, Option, A, OptionT](OptionT.apply _) _
+	def listT[F[_], A] = toT[F, List, A, ListT](ListT.apply _) _
 
 	def apply[A](λ: Context => A): Monitored[A] =
 		new Monitored[A] {
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -17,31 +17,57 @@ class ApplicationSpec extends Specification {
     import monitor.Monitored, Monitored._
 
     "optT" in {
-
       val f1 = Monitored(_ => Option("foo").point[Future])
       val f2 = Monitored(_ => Option(1).point[Future])
       val f3 = Monitored(_ => (None: Option[Int]).point[Future])
 
       val res = for {
-        e1 <- optT(f1)
-        e2 <- optT(f2)
+        e1 <- f1.T
+        e2 <- f2.T
       } yield (e1, e2)
 
       res.run(null).run must be_==(Some(("foo",1))).await
 
       val res2 = for {
-        e1 <- optT(f1)
-        e2 <- optT(f3)
+        e1 <- f1.T
+        e2 <- f3.T
       } yield (e1, e2)
 
       res2.run(null).run must be_==(None).await
 
       val res3 = for {
-        e1 <- optT(f3)
-        e2 <- optT(f2)
+        e1 <- f3.T
+        e2 <- f2.T
       } yield (e1, e2)
 
       res3.run(null).run must be_==(None).await
+    }
+
+    "listT" in {
+      val f1 = Monitored(_ => List("foo", "bar").point[Future])
+      val f2 = Monitored(_ => List(1, 2).point[Future])
+      val f3 = Monitored(_ => List[Int]().point[Future])
+
+      val res = for {
+        e1 <- f1.T
+        e2 <- f2.T
+      } yield (e1, e2)
+
+      res.run(null).run must be_==(List(("foo",1), ("foo",2), ("bar",1), ("bar",2))).await
+
+      val res2 = for {
+        e1 <- f1.T
+        e2 <- f3.T
+      } yield (e1, e2)
+
+      res2.run(null).run must be_==(List()).await
+
+      val res3 = for {
+        e1 <- f3.T
+        e2 <- f2.T
+      } yield (e1, e2)
+
+      res3.run(null).run must be_==(List()).await
     }
 
   }
