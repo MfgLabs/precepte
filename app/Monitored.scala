d99d1ff3cb0c2ecc81f3f88155a9435b8e81213d
jto <boudhevil@gmail.com>
2015-03-02 17:36:49 +0100

    Frustration

--- app/Monitored.scala
+++ app/Monitored.scala
@@ -3,6 +3,8 @@ package monitor
 import scala.concurrent.Future
 import scalaz.{ Hoist, Monad, Kleisli, OptionT, MonadTrans, ListT, EitherT, Applicative, Functor, ∨ }
 import scalaz.Leibniz.{===, refl}
+import scalaz.Id._
+import scalaz.Unapply
 
 trait HasHoist[M[_]] {
   type T[_[_], _]
@@ -29,49 +31,47 @@ object HasHoist {
   //   }
 }
 
-trait Monitored[C, FA] {
+trait Monitored[C, F[_], A] {
 
-  type F[_]
-  type A
+  val f: C => F[A]
+  def apply(c: C): F[A] = f(c)
 
-  /** Evidence that MA =:= M[A] */
-  def leibniz: FA === F[A]
+  def flatMap[B](fr: A => Monitored[C, F, B])(implicit m: Monad[F]) =
+    Monitored[C, F, B] { (c: C) =>
+      m.bind(apply(c))(a => fr(a)(c))
+    }
 
-  val f: C => FA
-  def apply(c: C) = f(c)
-  def run(c: C) = apply(c)
-
-  def flatMap[B](f: A => Monitored[C, F[B]])(implicit m: Monad[({ type λ[α] = Monitored[C, F[α]] })#λ]) =
-    m.bind(this.asInstanceOf[Monitored[C, F[A]]])(f)
+  def map[B](fu: A => B)(implicit m: Functor[F]) =
+    Monitored[C, F, B] { (c: C) =>
+      m.map(f(c))(fu)
+    }
 }
 
 object Monitored {
   import scalaz.syntax.monad._
   import scalaz.Id._
+  import scalaz.Unapply
 
-  def apply1[C, F0[_], A0](λ: C => F0[A0]): Monitored[C, F0[A0]] =
-    new Monitored[C, F0[A0]] {
-      type F[X] = F0[X]
-      type A = A0
+
+  def apply0[C, A0](λ: C => A0): Monitored[C, Id, A0] =
+    apply[C, Id, A0](λ)
+
+  def apply[C, F0[_], A0](λ: C => F0[A0]): Monitored[C, F0, A0] =
+    new Monitored[C, F0, A0] {
       val f = λ
-      def leibniz = refl
     }
 
-  def apply0[C, A0](λ: C => A0) =
-    apply1[C, Id, A0](λ)
 
-  def trans[C, F[_], G[_], A](m: Monitored[C, F[G[A]]])(implicit hh: HasHoist[G]) =
-    Monitored.apply1[C, ({ type λ[α] = hh.T[F, α] })#λ, A] { c =>
+  def trans[C, F[_], G[_], A](m: Monitored[C, F, G[A]])(implicit hh: HasHoist[G]) =
+    Monitored[C, ({ type λ[α] = hh.T[F, α] })#λ, A] { (c: C) =>
       hh.lift[F, A](m.f(c))
     }
 
 
-  implicit def monitoredInstances[C, F[_]: Monad, A] =
-    new Monad[({ type λ[α] = Monitored[C, F[α]] })#λ] {
-      def point[A](a: => A) = Monitored.apply1[C, F, A](_ => implicitly[Monad[F]].point(a))
-      def bind[A, B](m: Monitored[C, F[A]])(f: A => Monitored[C, F[B]]) =
-        Monitored.apply1[C, F, B] { c =>
-          m(c).flatMap(f(_)(c))
-        }
-    }
+  // implicit def monitoredInstances[C, F[_]: Monad, A] =
+  //   new Monad[({ type λ[α] = Monitored[C, F[α]] })#λ] {
+  //     def point[A](a: => A): Monitored[C, F[A]] = Monitored.apply1[C, F, A](_ => implicitly[Monad[F]].point(a))
+  //     def bind[A, B](m: Monitored[C, F[A]])(f: A => Monitored[C, F[B]]): Monitored[C, F[B]] =
+  //       m.flatMap(f)
+  //   }
 }
--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -5,6 +5,7 @@ import org.junit.runner._
 import play.api.test._
 import play.api.test.Helpers._
 
+
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
 
@@ -19,234 +20,199 @@ class ApplicationSpec extends Specification {
     import scalaz.std.scalaFuture._
     import scalaz.std.option._
     import scalaz.syntax.monad._
-
-    import scalaz.Monad
+    import scalaz.{ Kleisli, OptionT, EitherT }
 
     "Simple" in {
       def f1 = Monitored.apply0{(_: Log) => 1}
       def f2(i: Int) = Monitored.apply0{(_: Log) => s"foo $i"}
 
-      f1.flatMap(f2)
-
       val res = for {
         i <- f1
         r <- f2(i)
       } yield r
 
-      res.run(null) must be_==("foo 1")
-    }
-  }
-}
-
-/*
-@RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
-
-  import monitor.{ Monitored, Context }, Monitored._
-  trait Log {
-    def debug(s: String): Unit
-  }
-
-  "Application" should {
-    import play.api.libs.concurrent.Execution.Implicits._
-    import scala.concurrent.Future
-    import scalaz.std.scalaFuture._
-    import scalaz.std.option._
-    import scalaz.syntax.monad._
-    import scalaz.{ Kleisli, OptionT, EitherT }
-
-    "Simple" in {
-      def f1 = Monitored{(_: Context[Log]) => 1}
-      def f2(i: Int) = Monitored{(_: Context[Log]) => s"foo $i"}
-
-      val res = for {
-        i <- f1
-        r <- f2(i)
-      } yield r
-
-      res.run(null) must be_==("foo 1")
+      res(null) must be_==("foo 1")
     }
 
     "optT" in {
-      val f1 = Monitored((_: Context[Log]) => Option("foo").point[Future])
-      val f2 = Monitored((_: Context[Log]) => Option(1).point[Future])
-      val f3 = Monitored((_: Context[Log]) => (None: Option[Int]).point[Future])
+      val f1 = Monitored((_: Log) => Option("foo").point[Future])
+      val f2 = Monitored((_: Log) => Option(1).point[Future])
+      val f3 = Monitored((_: Log) => (None: Option[Int]).point[Future])
 
       val res = for {
-        e1 <- f1.T
-        e2 <- f2.T
+        e1 <- trans(f1)
+        e2 <- trans(f2)
       } yield (e1, e2)
 
-      res.run(null).run must be_==(Some(("foo",1))).await
+      res(null).run must be_==(Some(("foo",1))).await
 
-      val res2 = for {
-        e1 <- f1.T
-        e2 <- f3.T
-      } yield (e1, e2)
+      // val res2 = for {
+      //   e1 <- f1.T
+      //   e2 <- f3.T
+      // } yield (e1, e2)
 
-      res2.run(null).run must be_==(None).await
+      // res2.run(null).run must be_==(None).await
 
-      val res3 = for {
-        e1 <- f3.T
-        e2 <- f2.T
-      } yield (e1, e2)
+      // val res3 = for {
+      //   e1 <- f3.T
+      //   e2 <- f2.T
+      // } yield (e1, e2)
 
-      res3.run(null).run must be_==(None).await
+      // res3.run(null).run must be_==(None).await
     }
 
-    "listT" in {
-      val f1 = Monitored((_: Context[Log]) => List("foo", "bar").point[Future])
-      val f2 = Monitored((_: Context[Log]) => List(1, 2).point[Future])
-      val f3 = Monitored((_: Context[Log]) => List[Int]().point[Future])
+    // "listT" in {
+    //   val f1 = Monitored((_: Context[Log]) => List("foo", "bar").point[Future])
+    //   val f2 = Monitored((_: Context[Log]) => List(1, 2).point[Future])
+    //   val f3 = Monitored((_: Context[Log]) => List[Int]().point[Future])
 
-      val res = for {
-        e1 <- f1.T
-        e2 <- f2.T
-      } yield (e1, e2)
+    //   val res = for {
+    //     e1 <- f1.T
+    //     e2 <- f2.T
+    //   } yield (e1, e2)
 
-      res.run(null).run must be_==(List(("foo",1), ("foo",2), ("bar",1), ("bar",2))).await
+    //   res.run(null).run must be_==(List(("foo",1), ("foo",2), ("bar",1), ("bar",2))).await
 
-      val res2 = for {
-        e1 <- f1.T
-        e2 <- f3.T
-      } yield (e1, e2)
+    //   val res2 = for {
+    //     e1 <- f1.T
+    //     e2 <- f3.T
+    //   } yield (e1, e2)
 
-      res2.run(null).run must be_==(List()).await
+    //   res2.run(null).run must be_==(List()).await
 
-      val res3 = for {
-        e1 <- f3.T
-        e2 <- f2.T
-      } yield (e1, e2)
+    //   val res3 = for {
+    //     e1 <- f3.T
+    //     e2 <- f2.T
+    //   } yield (e1, e2)
 
-      res3.run(null).run must be_==(List()).await
-    }
+    //   res3.run(null).run must be_==(List()).await
+    // }
 
-    "EitherT" in {
-      import scalaz.{ \/ , \/-, -\/}
-      import EitherT.eitherTFunctor
+    // "EitherT" in {
+    //   import scalaz.{ \/ , \/-, -\/}
+    //   import EitherT.eitherTFunctor
 
-      val f1: Monitored[Log, Future[String \/ String]] =
-        Monitored(_ => \/-("foo").point[Future])
-      val f2: Monitored[Log, Future[String \/ Int]] =
-        Monitored(_ => \/-(1).point[Future])
-      val f3: Monitored[Log, Future[String \/ String]] =
-        Monitored(_ => -\/("Error").point[Future])
+    //   val f1: Monitored[Log, Future[String \/ String]] =
+    //     Monitored(_ => \/-("foo").point[Future])
+    //   val f2: Monitored[Log, Future[String \/ Int]] =
+    //     Monitored(_ => \/-(1).point[Future])
+    //   val f3: Monitored[Log, Future[String \/ String]] =
+    //     Monitored(_ => -\/("Error").point[Future])
 
-      val res = for {
-        e1 <- f1.T
-        e2 <- f2.T
-      } yield (e1, e2)
+    //   val res = for {
+    //     e1 <- f1.T
+    //     e2 <- f2.T
+    //   } yield (e1, e2)
 
-      res.run(null).run must be_==(\/-("foo" -> 1)).await
+    //   res.run(null).run must be_==(\/-("foo" -> 1)).await
 
-      val error = -\/("Error")
-      val res2 = for {
-        e1 <- f1.T
-        e2 <- f3.T
-      } yield (e1, e2)
+    //   val error = -\/("Error")
+    //   val res2 = for {
+    //     e1 <- f1.T
+    //     e2 <- f3.T
+    //   } yield (e1, e2)
 
-      res2.run(null).run must be_==(error).await
+    //   res2.run(null).run must be_==(error).await
 
-      val res3 = for {
-        e1 <- f3.T
-        e2 <- f2.T
-      } yield (e1, e2)
+    //   val res3 = for {
+    //     e1 <- f3.T
+    //     e2 <- f2.T
+    //   } yield (e1, e2)
 
-      res3.run(null).run must be_==(error).await
-    }
+    //   res3.run(null).run must be_==(error).await
+    // }
 
-    case class Board(pin: Option[Int])
-    object BoardComp {
-      def get(): Monitored[Log, Future[Board]] = Monitored{ c =>
-        c.value.debug("BoardComp.get")
-        Board(Some(1)).point[Future]
-      }
-    }
+    // case class Board(pin: Option[Int])
+    // object BoardComp {
+    //   def get(): Monitored[Log, Future[Board]] = Monitored{ c =>
+    //     c.value.debug("BoardComp.get")
+    //     Board(Some(1)).point[Future]
+    //   }
+    // }
 
-    case class Community(name: String)
-    case class Card(name: String)
+    // case class Community(name: String)
+    // case class Card(name: String)
 
-    object CardComp {
-      def getPin(id: Int): Monitored[Log, Future[Option[(Int, Card)]]] = Monitored { c =>
-        c.value.debug("CardComp.getPin")
-        Some(1 -> Card("card 1")).point[Future]
-      }
+    // object CardComp {
+    //   def getPin(id: Int): Monitored[Log, Future[Option[(Int, Card)]]] = Monitored { c =>
+    //     c.value.debug("CardComp.getPin")
+    //     Some(1 -> Card("card 1")).point[Future]
+    //   }
 
-      def countAll(): Monitored[Log, Future[Set[String]]] = Monitored { c =>
-        c.value.debug("CardComp.countAll")
-        Set("Edito", "Video").point[Future]
-      }
+    //   def countAll(): Monitored[Log, Future[Set[String]]] = Monitored { c =>
+    //     c.value.debug("CardComp.countAll")
+    //     Set("Edito", "Video").point[Future]
+    //   }
 
-      def rank(): Monitored[Log, Future[List[(Int, Card)]]] = Monitored { c =>
-        c.value.debug("CardComp.rank")
-        List(1 -> Card("foo"), 1 -> Card("bar")).point[Future]
-      }
+    //   def rank(): Monitored[Log, Future[List[(Int, Card)]]] = Monitored { c =>
+    //     c.value.debug("CardComp.rank")
+    //     List(1 -> Card("foo"), 1 -> Card("bar")).point[Future]
+    //   }
 
-      def cardsInfos(cs: List[(Int, Card)], pin: Option[Int]): Monitored[Log, Future[List[(Card, List[Community])]]] = Monitored { c =>
-        c.value.debug("CardComp.cardsInfos")
-        List(
-          Card("foo") -> List(Community("community 1"), Community("community 2")),
-          Card("bar") -> List(Community("community 2"))).point[Future]
-      }
-    }
+    //   def cardsInfos(cs: List[(Int, Card)], pin: Option[Int]): Monitored[Log, Future[List[(Card, List[Community])]]] = Monitored { c =>
+    //     c.value.debug("CardComp.cardsInfos")
+    //     List(
+    //       Card("foo") -> List(Community("community 1"), Community("community 2")),
+    //       Card("bar") -> List(Community("community 2"))).point[Future]
+    //   }
+    // }
 
-    import java.net.URL
-    case class Highlight(title: String, cover: URL)
-    object HighlightComp {
-      def get(): Monitored[Log, Future[Highlight]] = Monitored { c =>
-        c.value.debug("HighlightComp.get")
-        Highlight("demo", new URL("http://nd04.jxs.cz/641/090/34f0421346_74727174_o2.png")).point[Future]
-      }
-    }
-
-
-    "real world wb.fr home" in {
-
-      val logs = scala.collection.mutable.ArrayBuffer[String]()
-
-      val logger = new Log {
-        def debug(s: String): Unit = logs += s"[DEBUG] $s"
-      }
-
-      val context = Context(logger)
-
-      val getPin =
-        (for {
-          b <- BoardComp.get().lift[Option].T
-          id <- Monitored((_: Context[Log]) => b.pin.point[Future]).T
-          pin <- CardComp.getPin(id).T
-        } yield pin).run
+    // import java.net.URL
+    // case class Highlight(title: String, cover: URL)
+    // object HighlightComp {
+    //   def get(): Monitored[Log, Future[Highlight]] = Monitored { c =>
+    //     c.value.debug("HighlightComp.get")
+    //     Highlight("demo", new URL("http://nd04.jxs.cz/641/090/34f0421346_74727174_o2.png")).point[Future]
+    //   }
+    // }
 
 
-      val res = for {
-        pin <- Monitored(getPin(_: Context[Log]).run).K
-        cs <- CardComp.rank().K
-        cards <- CardComp.cardsInfos(cs, pin.map(_._1)).K
-        availableTypes <- CardComp.countAll().K
-        h <- HighlightComp.get().K
-      } yield (pin, cs, cards, availableTypes, h)
+    // "real world wb.fr home" in {
 
-      res.run(context) must be_==(
-        (Some((1, Card("card 1"))),
-          List((1, Card("foo")), (1, Card("bar"))),
-          List(
-            (Card("foo"), List(
-              Community("community 1"),
-              Community("community 2"))),
-            (Card("bar"),List(
-              Community("community 2")))),
-          Set("Edito", "Video"),
-          Highlight("demo", new URL("http://nd04.jxs.cz/641/090/34f0421346_74727174_o2.png")))
-      ).await
+    //   val logs = scala.collection.mutable.ArrayBuffer[String]()
 
-      logs must be_==(Seq(
-        "[DEBUG] BoardComp.get",
-        "[DEBUG] CardComp.getPin",
-        "[DEBUG] CardComp.rank",
-        "[DEBUG] CardComp.cardsInfos",
-        "[DEBUG] CardComp.countAll",
-        "[DEBUG] HighlightComp.get"))
-    }
+    //   val logger = new Log {
+    //     def debug(s: String): Unit = logs += s"[DEBUG] $s"
+    //   }
+
+    //   val context = Context(logger)
+
+    //   val getPin =
+    //     (for {
+    //       b <- BoardComp.get().lift[Option].T
+    //       id <- Monitored((_: Context[Log]) => b.pin.point[Future]).T
+    //       pin <- CardComp.getPin(id).T
+    //     } yield pin).run
+
+
+    //   val res = for {
+    //     pin <- Monitored(getPin(_: Context[Log]).run).K
+    //     cs <- CardComp.rank().K
+    //     cards <- CardComp.cardsInfos(cs, pin.map(_._1)).K
+    //     availableTypes <- CardComp.countAll().K
+    //     h <- HighlightComp.get().K
+    //   } yield (pin, cs, cards, availableTypes, h)
+
+    //   res.run(context) must be_==(
+    //     (Some((1, Card("card 1"))),
+    //       List((1, Card("foo")), (1, Card("bar"))),
+    //       List(
+    //         (Card("foo"), List(
+    //           Community("community 1"),
+    //           Community("community 2"))),
+    //         (Card("bar"),List(
+    //           Community("community 2")))),
+    //       Set("Edito", "Video"),
+    //       Highlight("demo", new URL("http://nd04.jxs.cz/641/090/34f0421346_74727174_o2.png")))
+    //   ).await
+
+    //   logs must be_==(Seq(
+    //     "[DEBUG] BoardComp.get",
+    //     "[DEBUG] CardComp.getPin",
+    //     "[DEBUG] CardComp.rank",
+    //     "[DEBUG] CardComp.cardsInfos",
+    //     "[DEBUG] CardComp.countAll",
+    //     "[DEBUG] HighlightComp.get"))
+    // }
   }
 }
-*/
