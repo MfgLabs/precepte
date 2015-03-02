e827a01b7dec61cb9e8a8c7f167e93b4947cc6ce
jto <boudhevil@gmail.com>
2015-03-02 12:15:38 +0100

    WIP

--- app/Monitored.scala
+++ app/Monitored.scala
@@ -1,96 +1,69 @@
 package monitor
 
 import scala.concurrent.Future
-import scalaz.{ Monad, Kleisli, OptionT, MonadTrans, ListT, EitherT, Applicative, Functor, ∨ }
+import scalaz.{ Hoist, Monad, Kleisli, OptionT, MonadTrans, ListT, EitherT, Applicative, Functor, ∨ }
 
-trait Monitored[C, A] {
-  val f: Context[C] => A
-  def apply(c: Context[C]) = f(c)
-  def run(c: Context[C]) = apply(c)
+trait HasHoist[M[_]] {
+  type T[_[_], _]
+  def lift[F[_], A](f: F[M[A]]): T[F, A]
 }
 
-case class Context[C](value: C)
+object HasHoist {
+  def apply[M[_]](implicit h: HasHoist[M]): HasHoist[M] = h
 
-trait OptionTMonitored[C, F[_], A] extends Monitored[C, F[Option[A]]] {
-  def T = Monitored.optT[C, F, A](this)
+  implicit def optionHasHoist =
+  new HasHoist[Option] {
+    type T[F[_], A] = OptionT[F, A]
+    def lift[F[_], A](f: F[Option[A]]): OptionT[F, A] = OptionT.apply(f)
+  }
+
+  // implicit def listHasHoist(implicit h: Hoist[ListT]) = new HasHoist[List, ListT] {
+  //   def hoist = h
+  //   def lift[F[_], A](f: F[List[A]]): ListT[F, A] = ListT.apply(f)
+  // }
+
+  // implicit def eitherHasHoist[A](implicit h: Hoist[({ type λ[α[_], β] = EitherT[α, A, β] })#λ]) =
+  //   new HasHoist[({ type λ[α] = A ∨ α })#λ, ({ type λ[α[_], β] = EitherT[α, A, β] })#λ] {
+  //     def hoist = h
+  //     def lift[F[_], B](f: F[A ∨ B]): EitherT[F, A, B] = EitherT.apply(f)
+  //   }
 }
 
-trait ListTMonitored[C, F[_], A] extends Monitored[C, F[List[A]]] {
-  def T = Monitored.listT[C, F, A](this)
-}
+trait Monitored[C, FA] {
 
-trait EitherTMonitored[C, F[_], A, B] extends Monitored[C, F[A ∨ B]] {
-  def T = Monitored.eitherT[C, F, A, B](this)
-}
+  type F[_]
+  type A
 
-trait LiftableMonitored[C, F[_], A] extends Monitored[C, F[A]] {
-  def lift[G[_]: Applicative](implicit fu: Functor[F]) =
-    Monitored.lift[G].apply(this)
-}
+  val f: C => FA
+  def apply(c: C) = f(c)
+  def run(c: C) = apply(c)
 
-trait ToKleisliMonitored[C, F[_], A] extends Monitored[C, F[A]] {
-  def K = Monitored.toK(this)
+  def T(implicit hh: HasHoist[F]) = ???
 }
 
 object Monitored {
   import scalaz.syntax.monad._
 
-  implicit def toOptionT[C, F[_], A](m: Monitored[C, F[Option[A]]]) = new OptionTMonitored[C, F, A] {
-    val f = m.f
-  }
-
-  implicit def toListT[C, F[_], A](m: Monitored[C, F[List[A]]]) = new ListTMonitored[C, F, A] {
-    val f = m.f
-  }
-
-  implicit def toEitherT[C, F[_], A, B](m: Monitored[C, F[A ∨ B]]) = new EitherTMonitored[C, F, A, B] {
-    val f = m.f
-  }
-
-  implicit def toLiftableT[C, F[_], A](m: Monitored[C, F[A]]) = new LiftableMonitored[C, F, A] {
-    val f = m.f
-  }
-
-  implicit def toToKleisliMonitored[C, F[_], A](m: Monitored[C, F[A]]) = new ToKleisliMonitored[C, F, A] {
-    val f = m.f
-  }
-
-  def toT[C, F[_], G[_], A, T[_[_], _]](f: F[G[A]] => T[F, A])(m: Monitored[C, F[G[A]]]): Kleisli[({ type λ[α] = T[F, α] })#λ, Context[C], A] = {
-    type Trans[α] = T[F, α]
-    toK(m).mapK[Trans, A](f)
-  }
-
-  def toK[C, F[_], A](m: Monitored[C, F[A]]) =
-    Kleisli(m.f)
-
-  def optT[C, F[_], A] = toT[C, F, Option, A, OptionT](OptionT.apply _) _
-  def listT[C, F[_], A] = toT[C, F, List, A, ListT](ListT.apply _) _
-
-  def eitherT[C, F[_], A, B](m: Monitored[C, F[A ∨ B]]): Kleisli[({ type λ[α] = EitherT[F, A, α] })#λ, Context[C], B] = {
-    type Trans[α] = EitherT[F, A, α]
-    toK(m).mapK[Trans, B](EitherT.apply _)
-  }
-
-
-  trait Deferred[G[_]] {
-    def apply[C, F[_]: Functor, A](m: Monitored[C, F[A]]): Monitored[C, F[G[A]]]
-  }
-  def lift[G[_]: Applicative] = new Deferred[G] {
-    def apply[C, F[_]: Functor, A](m: Monitored[C, F[A]]): Monitored[C, F[G[A]]] =
-      m.map{_.map{ a => implicitly[Applicative[G]].point[A](a) }}
-  }
-
-  def apply[C, A](λ: Context[C] => A): Monitored[C, A] =
-    new Monitored[C, A] { val f = λ }
-
-  def id(): Id = ???
-  trait Id
-
-  implicit def monitoredInstances[C, A]: Monad[({ type λ[α] = Monitored[C, α] })#λ] =
-    new Monad[({ type λ[α] = Monitored[C, α] })#λ] {
-      def point[A](a: => A) = Monitored(_ => a)
-      def bind[A, B](m: Monitored[C, A])(f: A => Monitored[C, B]) =
-        Monitored(c => f(m(c))(c))
+  def apply[F0[_], C, A0](λ: C => F0[A0]): Monitored[C, F0[A0]] =
+    new Monitored[C, F0[A0]] {
+      type F[X] = F0[X]
+      type A = A0
+      val f = λ
     }
+
+  def lift[C, A0](λ: C => A0): Monitored[C, A0] =
+    new Monitored[C, A0] {
+      type F[X] = scalaz.Id.Id[X]
+      type A = A0
+      val f = λ
+    }
+
+
+  // implicit def monitoredInstances[C, A]: Monad[({ type λ[α] = Monitored[C, α] })#λ] =
+  //   new Monad[({ type λ[α] = Monitored[C, α] })#λ] {
+  //     def point[A](a: => A) = Monitored(_ => a)
+  //     def bind[A, B](m: Monitored[C, A])(f: A => Monitored[C, B]) =
+  //       Monitored(c => f(m(c))(c))
+  //   }
 
 }
