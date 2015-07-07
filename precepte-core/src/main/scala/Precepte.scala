05356f554107f1f4389190659df7abed6fa459c5
Pascal Voitot <pascal.voitot.dev@gmail.com>
2015-07-07 08:58:41 +0200

    better iso

--- precepte-core/src/main/scala/Precepte.scala
+++ precepte-core/src/main/scala/Precepte.scala
@@ -3,7 +3,7 @@ package precepte
 
 import scala.language.higherKinds
 import scalaz.{ Bind, Monad, MonadPlus, Applicative, Functor, \/, \/-, -\/, IndexedStateT, StateT, State, Traverse, ~> }
-import scalaz.Isomorphism.<~>
+import scalaz.Isomorphism.{<~>, <=>, isoRefl, isoNaturalRefl}
 import scalaz.syntax.monad._
 
 import scala.annotation.tailrec
@@ -21,9 +21,81 @@ class TaggingContext[Tags, ManagedState, UnmanagedState, S <: PState0[Tags, Mana
     val iso: self.Precepte <~> tc.Precepte
   }
 
+  trait TaggingContextIso0[Tags, ManagedState, UnmanagedState, S <: PState0[Tags, ManagedState, UnmanagedState], F2[_]] {
 
-  def iso[F2[_]](isoF: F <~> F2)(implicit mf: Monad[F], mf2: Monad[F2]) = new TaggingContextIso[F2] {
-    override val tc = new TaggingContext[Tags, ManagedState, UnmanagedState, S, F2]
+    val tc: TaggingContext[Tags, ManagedState, UnmanagedState, S, F2]
+
+    val iso: self.Precepte <~> tc.Precepte
+  }
+
+  // def iso[F2[_]](isoF: F <~> F2)(implicit mf: Monad[F], mf2: Monad[F2]) = new TaggingContextIso[F2] {
+  //   override val tc = new TaggingContext[Tags, ManagedState, UnmanagedState, S, F2]
+
+  //   override lazy val iso: self.Precepte <~> tc.Precepte = new (self.Precepte <~> tc.Precepte) {
+
+  //     def to = new (self.Precepte ~> tc.Precepte) {
+  //       def apply[A](p: self.Precepte[A]): tc.Precepte[A] = p match {
+
+  //         case self.Return(a) =>
+  //           tc.Return(a)
+
+  //         case self.Step(st, tags) =>
+  //           tc.Step[A](
+  //             IndexedStateT { (s: S) =>
+  //               isoF.to(st(s).map{ case (s, p) => s -> iso.to(p) })
+  //             }, tags
+  //           )
+
+  //         case f@self.Flatmap(sub, next) =>
+  //           tc.Flatmap(() => iso.to(sub()), (i:f._I) => iso.to(next(i)))
+
+  //         case f@self.FlatmapK(subk, fk) =>
+  //           tc.FlatmapK(
+  //             iso.to(subk),
+  //             (f2: F2[(S, f._A)]) => iso.to(fk(isoF.from(f2)))
+  //           )
+
+  //       }
+  //     }
+
+  //     def from = new (tc.Precepte ~> self.Precepte) {
+  //       def apply[A](p2: tc.Precepte[A]): self.Precepte[A] = p2 match {
+
+  //         case tc.Return(a) =>
+  //           self.Return(a)
+
+  //         case tc.Step(st, tags) =>
+  //           self.Step[A](
+  //             IndexedStateT { (s: S) =>
+  //               isoF.from(st(s).map{ case (s, p) => s -> iso.from(p) })
+  //             }, tags
+  //           )
+
+  //         case f@tc.Flatmap(sub, next) =>
+  //           self.Flatmap(() => iso.from(sub()), (i:f._I) => iso.from(next(i)))
+
+  //         case f@tc.FlatmapK(subk, fk) =>
+  //           self.FlatmapK(
+  //             iso.from(subk),
+  //             (ff: F[(S, f._A)]) => iso.from(fk(isoF.to(ff)))
+  //           )
+
+  //       }
+  //     }
+  //   }
+  // }
+
+  def isoF[F2[_]](isoFF: F <~> F2)(implicit mf: Monad[F], mf2: Monad[F2]) =
+    iso0[Tags, ManagedState, UnmanagedState, S, F2](isoRefl, isoRefl, isoFF)
+
+  def isoState[Tags2, ManagedState2, UnmanagedState2, S2 <: PState0[Tags2, ManagedState2, UnmanagedState2]](isoT: Tags <=> Tags2, isoS: S <=> S2)(implicit mf: Monad[F]) =
+    iso0[Tags2, ManagedState2, UnmanagedState2, S2, F](isoT, isoS, isoNaturalRefl)
+
+  def iso0[Tags2, ManagedState2, UnmanagedState2, S2 <: PState0[Tags2, ManagedState2, UnmanagedState2], F2[_]](
+    isoT: Tags <=> Tags2, isoS: S <=> S2, isoF: F <~> F2
+  )(implicit mf: Monad[F], mf2: Monad[F2]) = new TaggingContextIso0[Tags2, ManagedState2, UnmanagedState2, S2, F2] {
+
+    override val tc = new TaggingContext[Tags2, ManagedState2, UnmanagedState2, S2, F2]
 
     override lazy val iso: self.Precepte <~> tc.Precepte = new (self.Precepte <~> tc.Precepte) {
 
@@ -35,9 +107,9 @@ class TaggingContext[Tags, ManagedState, UnmanagedState, S <: PState0[Tags, Mana
 
           case self.Step(st, tags) =>
             tc.Step[A](
-              IndexedStateT { (s: S) =>
-                isoF.to(st(s).map{ case (s, p) => s -> iso.to(p) })
-              }, tags
+              IndexedStateT { (s2: S2) =>
+                isoF.to(st(isoS.from(s2)).map{ case (s, p) => isoS.to(s) -> iso.to(p) })
+              }, isoT.to(tags)
             )
 
           case f@self.Flatmap(sub, next) =>
@@ -46,7 +118,7 @@ class TaggingContext[Tags, ManagedState, UnmanagedState, S <: PState0[Tags, Mana
           case f@self.FlatmapK(subk, fk) =>
             tc.FlatmapK(
               iso.to(subk),
-              (f2: F2[(S, f._A)]) => iso.to(fk(isoF.from(f2)))
+              (f2: F2[(S2, f._A)]) => iso.to(fk(isoF.from(f2.map{ case (s2, fa) => isoS.from(s2) -> fa })))
             )
 
         }
@@ -61,8 +133,8 @@ class TaggingContext[Tags, ManagedState, UnmanagedState, S <: PState0[Tags, Mana
           case tc.Step(st, tags) =>
             self.Step[A](
               IndexedStateT { (s: S) =>
-                isoF.from(st(s).map{ case (s, p) => s -> iso.from(p) })
-              }, tags
+                isoF.from(st(isoS.to(s)).map{ case (s, p) => isoS.from(s) -> iso.from(p) })
+              }, isoT.from(tags)
             )
 
           case f@tc.Flatmap(sub, next) =>
@@ -71,14 +143,13 @@ class TaggingContext[Tags, ManagedState, UnmanagedState, S <: PState0[Tags, Mana
           case f@tc.FlatmapK(subk, fk) =>
             self.FlatmapK(
               iso.from(subk),
-              (ff: F[(S, f._A)]) => iso.from(fk(isoF.to(ff)))
+              (ff: F[(S, f._A)]) => iso.from(fk(isoF.to(ff.map { case (s, fa) => isoS.to(s) -> fa })))
             )
 
         }
       }
     }
   }
-
 
   trait ResumeStep[A]
   case class FlatMapStep[A](v: F[(Precepte[A], S)]) extends ResumeStep[A]
@@ -125,7 +196,6 @@ class TaggingContext[Tags, ManagedState, UnmanagedState, S <: PState0[Tags, Mana
       case Step(st, tags) =>
         // println(s"EVAL S $this")
         val state0 = upd.appendTags(state, tags)
-        // val (state0, ids0) = ps.run(state, ids, tags)
         // append tags to managed state and propagate this new managed state to next step
         FlatMapStep(st.run(state0).map { case (s, p) => (p, s) })
 
@@ -140,7 +210,7 @@ class TaggingContext[Tags, ManagedState, UnmanagedState, S <: PState0[Tags, Mana
             // println("EVAL Flatmap - Step")
             val state0 = upd.appendTags(state, tags)
             // repass state as a Step in a Flatmap means the flatMap chain is finished
-            // DO NOT APPEND TAGS TO
+            // Do not reuse appended segment but original state
             FlatMapStep(st.run(state0).map { case (s, p) =>
               (p.flatMap(next), upd.updateUnmanaged(state, s.unmanaged))
             })
