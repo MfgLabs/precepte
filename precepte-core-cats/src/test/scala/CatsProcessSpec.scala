98310a56dffd5f11f977ccf593ec5c7609a94742
mandubian <pascal@projectseptember.com>
2015-10-02 10:46:05 -0400

    sample

--- precepte-core-cats/src/test/scala/CatsProcessSpec.scala
+++ precepte-core-cats/src/test/scala/CatsProcessSpec.scala
@@ -28,95 +28,55 @@ class CatsProcessSpec extends FlatSpec with ScalaFutures {
 
   import default._
 
-  type Pre[A] = DefaultPre[Future, Unit, A]
+  type Pre[A] = DefaultPre[Future, Int, A]
 
-  val env = BaseEnv(Host("localhost"), Environment.Test, Version("1.0"))
 
   private def tags(n: String) = BaseTags(Callee(n), Category.Database)
 
   it should "observe" in {
-    def nostate = ST(Span.gen, env, Vector.empty, 0)
 
     implicit val intSG = new cats.Semigroup[Int] {
       def combine(f1: Int, f2: Int) = f1 + f2
     }
 
-    // val canbe = Precepte(tags("can be")).apply((s: ST[Int]) => Future(1))
-    // val persian = Precepte(tags("persian")).apply((s: ST[Int]) => Future(0))
-    // val meets = Precepte(tags("meets")).apply((s: ST[Int]) => Future(1))
-    // val sphynx = Precepte(tags("sphynx")).apply((s: ST[Int]) => Future(2))
-    // val gives = Precepte(tags("gives")).apply((s: ST[Int]) => Future(2))
-    // val mainCoon = Precepte(tags("main coon")).apply((s: ST[Int]) => Future(3))
-    // val burmese = Precepte(tags("burmese")).apply((s: ST[Int]) => Future(4))
-    // val munchkin = Precepte(tags("munchkin")).apply((s: ST[Int]) => Future(5))
-    // val tabby = Precepte(tags("tabby")).apply((s: ST[Int]) => Future(6))
-    // val himalayan = Precepte(tags("himalayan")).apply((s: ST[Int]) => Future(7))
-    // val but = Precepte(tags("but in Précepte")).apply((s: ST[Int]) => Future(0))
-    // val none = Precepte(tags("none")).apply((s: ST[Int]) => Future(0))
-    // val is = Precepte(tags("is")).apply((s: ST[Int]) => Future(0))
-    // val aspureas = Precepte(tags("as pure as")).apply((s: ST[Int]) => Future(0))
-    // val kats = Precepte(tags("cats")).apply((s: ST[Int]) => Future(0))
-    // val animal = Precepte(tags("animal")).apply((s: ST[Int]) => Future(0))
-    // val framework = Precepte(tags("framework")).apply((s: ST[Int]) => Future(0))
-    // val purely = Precepte(tags("purely")).apply((s: ST[Int]) => Future(0))
-    // val functional = Precepte(tags("functional")).apply((s: ST[Int]) => Future(0))
-    // val typesafe = Precepte(tags("typesafe")).apply((s: ST[Int]) => Future(0))
-    // val api = Precepte(tags("api")).apply((s: ST[Int]) => Future(0))
-    // val theEnd = Precepte(tags("the end")).apply((s: ST[Int]) => Future(0))
-
-    val p8 =
+    val pre: Pre[Unit] =
       for {
-        _ <- Pre(tags("cats")).apply((s: ST[Int]) => Future(0))
-        _ <- Pre(tags("can be")).apply((s: ST[Int]) => Future(1))
-        _ <- (Pre(tags("persian")).apply((s: ST[Int]) => Future(2))
-                |@| Pre(tags("main coon")).apply((s: ST[Int]) => Future(3))
-                |@| Pre(tags("siamese")).apply((s: ST[Int]) => Future(4))
-                |@| Pre(tags("sphynx")).apply((s: ST[Int]) => Future(5))
-                |@| Pre(tags("burmese")).apply((s: ST[Int]) => Future(6))
-                |@| Pre(tags("munchkin")).apply((s: ST[Int]) => Future(7))
-                |@| Pre(tags("tabby")).apply((s: ST[Int]) => Future(8))
-                |@| Pre(tags("himalayan")).apply((s: ST[Int]) => Future(9))
+        _ <-  Pre(tags("cats")){ s:ST[Int] => Future(0) }
+        _ <-  Pre(tags("can be")){ s:ST[Int] => Future(1) }
+        _ <-  (    
+              Pre(tags("persian")){ s: ST[Int] => Future(2) }
+              |@| Pre(tags("main coon")){ s: ST[Int] => Future(3) }
+              |@| Pre(tags("siamese")){ s: ST[Int] => Future(4) }
+              |@| Pre(tags("sphynx")){ s: ST[Int] => Future(5) }
+              |@| Pre(tags("burmese")){ s: ST[Int] => Future(6) }
+              |@| Pre(tags("munchkin")){ s: ST[Int] => Future(7) }
+              |@| Pre(tags("tabby")){ s: ST[Int] => Future(8) }
+              |@| Pre(tags("himalayan")){ s: ST[Int] => Future(9) }
               ).tupled
-        _ <- Pre(tags("but")).apply((s: ST[Int]) => Future(10))
-        _ <- Pre(tags("none is")).apply((s: ST[Int]) => Future(11))
-        _ <- Pre(tags("as")).apply((s: ST[Int]) => Future(12))
-        _ <- (Pre(tags("purely")).apply((s: ST[Int]) => Future(13))
-              |@| Pre(tags("functional")).apply((s: ST[Int]) => Future(14))
-              |@| Pre(tags("typesafe")).apply((s: ST[Int]) => Future(15))).tupled
-        _ <- Pre(tags("as")).apply((s: ST[Int]) => Future(16))
-        _ <- Pre(tags("cats")).apply((s: ST[Int]) => Future(17))
+        _ <-  Pre(tags("but")){ s: ST[Int] => Future(10) }
+        _ <-  Pre(tags("none is")){ s: ST[Int] => Future(11) }
+        _ <-  Pre(tags("as")){ s: ST[Int] => Future(12) }
+        _ <-  (    
+              Pre(tags("purely")){ s: ST[Int] => Future(13) }
+              |@| Pre(tags("functional")){ s: ST[Int] => Future(14) }
+              |@| Pre(tags("typesafe")){ s: ST[Int] => Future(15) }
+              ).tupled
+        _ <-  Pre(tags("as")){ s: ST[Int] => Future(16) }
+        _ <-  (
+              Pre(tags("cats")){ s: ST[Int] => Future(17) }
+              |@| Pre(tags("with")){ s: ST[Int] => Future(18) }
+              |@| Pre(tags("précepte")){ s: ST[Int] => Future(19) }
+              ).tupled
       } yield ()
 
-    // val p0 = Precepte(tags("p0")).applyU((s: ST[Int]) => Future(0 -> 0))
-    // val p1 = Precepte(tags("p1")).applyU((s: ST[Int]) => Future(1 -> 1))
-    // val p2 = Precepte(tags("p2")).applyU((s: ST[Int]) => Future(2 -> 2))
-    // val p3 = Precepte(tags("p3")).applyU((s: ST[Int]) => Future(3 -> 3))
-    // val p4 = Precepte(tags("p4")).applyU((s: ST[Int]) => Future(4 -> 4))
-    // val p5 = Precepte(tags("p5")).applyU((s: ST[Int]) => Future(5 -> 5))
-    // val p6 = Precepte(tags("p6")).applyU((s: ST[Int]) => Future(6 -> 6))
-    // val p7 = Precepte(tags("p7")).applyU((s: ST[Int]) => Future(7 -> 7))
+    val env = BaseEnv(Host("localhost"), Environment.Test, Version("1.0"))
+    val state0 = ST(Span.gen, env, Vector.empty, 0)
 
-    // val p8 =
-    //   for {
-    //   // _ <- p0
-    //   // _ <- (p1 |@| p2 |@| p3).tupled
-    //   _ <- Precepte(tags("sub"))(p4)
-    //   // _ <- p5
-    //   // _ <- Precepte(tags("sub2"))(for {
-    //   //     _ <- (p1 |@| p2 |@| p3 |@| p4).tupled
-    //   //     _ <- p6
-    //   //     _ <- (p4 |@| p5 |@| Precepte(tags("sub3"))(p6)).tupled
-    //   //     _ <- p7
-    //   //   } yield ())
-    // } yield ()
-
-
-    val (ssubap2, _) =
-      p8
+    val (graph, _) = pre
         .graph(Graph.empty)
-        .eval(nostate)
+        .eval(state0)
         .futureValue
-    println(ssubap2.viz)
+    println(graph.viz)
 
   }
 }
