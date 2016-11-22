1b6a2a38184203c2762ce7a34e97b353c8f6c151
jto <boudhevil@gmail.com>
2016-11-22 14:19:28 +0100

    Stack safe function composition

--- precepte-core/src/main/scala/Precepte.scala
+++ precepte-core/src/main/scala/Precepte.scala
@@ -23,28 +23,42 @@ import scala.annotation.tailrec
 /**
 * Stack safe function composition
 */
-private[precepte] class Compose1[T1, R] private (val fs: Array[(Any => Any, Int)]) extends (T1 => R) {
+private[precepte] final class Compose1[T1, R] private (val fs: Array[(Any => Any, Int)]) extends (T1 => R) {
 
   val MAX_DEPTH = 1000
 
-  @inline private def add(fd: (Any => Any, Int), gs: Array[(Any => Any, Int)]) = {
+  private def append(gs: Array[(Any => Any, Int)], fd: (Any => Any, Int)) = {
     val (fl, dl) = gs.last
     val (f, d) = fd
     if(d + dl > MAX_DEPTH) {
       gs :+ fd
     } else {
-      val nf = (a: Any) => fl(f(a))
+      val nf = (a: Any) => f(fl(a))
       gs.init :+ ((nf, d + dl))
     }
   }
 
+  private def prepend(fd: (Any => Any, Int), gs: Array[(Any => Any, Int)]) = {
+    val (fl, dl) = gs.head
+    val (f, d) = fd
+    if(d + dl > MAX_DEPTH) {
+      fd +: gs
+    } else {
+      val nf = (a: Any) => fl(f(a))
+      ((nf, d + dl)) +: gs.init
+    }
+  }
+
+  private def concat(hs: Array[(Any => Any, Int)], ts: Array[(Any => Any, Int)]) =
+    hs.init ++ prepend(hs.last, ts)
+
   @inline private def build = (i0: T1) => {
     var i = i0.asInstanceOf[Any]
-    var j = 0
-    while(j < fs.length) {
+    var j = fs.length - 1
+    while(j >= 0) {
       val (f, _) = fs(j)
       i = f(i)
-      j += 1
+      j -= 1
     }
     i.asInstanceOf[R]
   }
@@ -52,24 +66,29 @@ private[precepte] class Compose1[T1, R] private (val fs: Array[(Any => Any, Int)
   override def compose[A](g: A => T1): A => R =
     g match {
       case Compose1(gfs) =>
-        new Compose1(gfs ++ fs)
+        new Compose1(concat(gfs, fs))
       case _ =>
         val fd = (g.asInstanceOf[Any => Any], 1)
-        new Compose1(add(fd, fs))
+        new Compose1(prepend(fd, fs))
     }
 
   override def andThen[A](g: R => A): T1 => A =
-    g compose this
+    g match {
+      case Compose1(gfs) =>
+        new Compose1(concat(fs, gfs))
+      case _ =>
+        val fd = (g.asInstanceOf[Any => Any], 1)
+        new Compose1(append(fs, fd))
+    }
 
   def apply(a: T1): R = build(a)
 }
 
 object Compose1 {
-  def apply[T1, B, R](f0: T1 => B, f1: B => R): T1 => R =
-    (f0, f1) match {
-      case (f00 @ Compose1(_), f01) => f00 andThen f01
-      case (f00, f01 @ Compose1(_)) => f01 compose f00
-      case (f00, f01) => new Compose1(Array(f00.asInstanceOf[Any => Any] -> 1, f01.asInstanceOf[Any => Any] -> 1))
+  def apply[T1, R](f0: T1 => R): T1 => R =
+    f0 match {
+      case Compose1(_) => f0
+      case f => new Compose1(Array((f.asInstanceOf[Any => Any], 1)))
     }
 
   def unapply[T1, R](c: Compose1[T1, R]) =
@@ -176,8 +195,7 @@ sealed trait Precepte[Ta, ManagedState, UnmanagedState, F[_], A] {
 
       case fm @ SMap(sub, pf) =>
         if(d < maxDepth) {
-          step(sub, Compose1(pf, f), d + 1)
-          // step(sub, (a: fm._I) => f(pf(a)), d + 1)
+          step(sub, Compose1(pf).andThen(f), d + 1)
         } else {
           sub.flatMap(a => f(pf(a)))
         }
@@ -257,7 +275,7 @@ sealed trait Precepte[Ta, ManagedState, UnmanagedState, F[_], A] {
             NextStep(fs, next)
 
           case SMap(sub2, f2) =>
-            sub2.fastFlatMap(Compose1(f2, next)).resume(idx)(state)
+            sub2.fastFlatMap(Compose1(f2).andThen(next)).resume(idx)(state)
 
           case f@Flatmap(sub2, next2) =>
             sub2.fastFlatMap(z => next2(z).fastFlatMap(next)).resume(idx)(state)
